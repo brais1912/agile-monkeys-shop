5875a913e8acfc17ee6a7fc9d3f8d6e2ff841a88
Brais González Úbeda <brais.gonzalez@grupoavalon.es>
2022-08-06 11:38:09 +0200

    Initial app structure

---
+++ agile-monkeys-shop-boot/src/main/java/com/brais/agilemonkeysshop/AgileMonkeysShopApplication.java
@@ -0,0 +1,13 @@
+package com.brais.agilemonkeysshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgileMonkeysShopApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(AgileMonkeysShopApplication.class, args);
+  }
+
+}
---
+++ agile-monkeys-shop-boot/src/test/java/com/brais/agilemonkeysshop/AgileMonkeysShopApplicationTests.java
@@ -0,0 +1,14 @@
+package com.brais.agilemonkeysshop;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgileMonkeysShopApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
