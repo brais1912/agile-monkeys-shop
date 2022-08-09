feb5e2a1d7abcec773cd2b031b24b33106e425cb
Brais González Úbeda <braisgu@gmail.com>
2022-08-09 19:38:47 +0200

    Checkstyle maven plugin application in all modules

--- agile-monkeys-shop-boot/src/test/java/com/brais/agilemonkeysshop/AgileMonkeysShopApplicationTests.java
+++ agile-monkeys-shop-boot/src/test/java/com/brais/agilemonkeysshop/AgileMonkeysShopApplicationTests.java
@@ -1,8 +1,8 @@
 package com.brais.agilemonkeysshop;
 
-import org.junit.jupiter.api.Test;
-
 import org.springframework.boot.test.context.SpringBootTest;
+
+import org.junit.jupiter.api.Test;
 
 @SpringBootTest
 class AgileMonkeysShopApplicationTests {
