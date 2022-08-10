0f310645eda360ea62121cef7c587a189f43f946
Brais González Úbeda <braisgu@gmail.com>
2022-08-10 18:33:24 +0200

    BCrypPasswordEncoder added

--- agile-monkeys-shop-infrastructure/src/main/java/com/brais/agilemonkeysshop/user/mapper/UserMongoMapper.java
+++ agile-monkeys-shop-infrastructure/src/main/java/com/brais/agilemonkeysshop/user/mapper/UserMongoMapper.java
@@ -3,14 +3,19 @@ package com.brais.agilemonkeysshop.user.mapper;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
 import com.brais.agilemonkeysshop.FullUser;
 import com.brais.agilemonkeysshop.user.UserStatusEnum;
 import com.brais.agilemonkeysshop.user.UserTypeEnum;
 import com.brais.agilemonkeysshop.user.entity.User;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper
 public interface UserMongoMapper {
+
+  BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
 
   List<FullUser> toFullUserList(List<User> userList);
 
@@ -24,5 +29,6 @@ public interface UserMongoMapper {
 
   FullUser toFullUser(User user);
 
+  @Mapping(target = "password", expression = "java(bCryptPasswordEncoder.encode(fullUser.password()))")
   User toUser(FullUser fullUser);
 }
